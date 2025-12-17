0d25f812097f12d51b7a01f9a0dc53995bdac3d4
Rabin Khandakar <rabinbd22@gmail.com>
2025-12-17 15:45:13 +0600

    Integrate React Router into main application

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import AuthProvider from "./providers/AuthProvider";
 import QueryProvider from "./providers/QueryProvider";
 import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router";
+import router from "./routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryProvider>
-        <h1 className="text-4xl font-bold text-center mt-10">Club Sphere</h1>
+        <RouterProvider router={router} />
         <Toaster position="top-right" />
       </QueryProvider>
     </AuthProvider>
